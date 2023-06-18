fee139607a5828be91c3c1f5a349f77a04f00550
Jose Luis Campos <joseluiscamposrodriguez@hotmail.com>
2023-06-18 07:58:46 -0600

    add get storage

--- lib/src/controller/login_controller.dart
+++ lib/src/controller/login_controller.dart
@@ -1,16 +1,30 @@
+import 'package:death_dream/src/providers/users_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import '../models/response_api.dart';
 
 class LoginController extends GetxController {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
-  void login() {
+  UsersProvider usersProvider = UsersProvider();
+
+  void login() async {
     String email = emailController.text.trim();
     String password = passwordController.text.trim();
-    if(isValidForm(email, password)){
-      Get.snackbar('Success', 'Datos correctos');
-      goToHome();
+    if (isValidForm(email, password)) {
+      ResponseApi responseApi = await usersProvider.login(email, password);
+      print('ResponseApi${responseApi.toJson()}');
+      if (responseApi.success==true) {
+        Get.snackbar('Success', '${responseApi.message}',
+            backgroundColor: Colors.greenAccent);
+        goToHome();
+      } else {
+        Get.snackbar('Error', '${responseApi.message}',
+            backgroundColor: Colors.redAccent);
+      }
+      
     }
   }
 
@@ -25,14 +39,14 @@ class LoginController extends GetxController {
       Get.snackbar('Error', 'La contraseña debe tener al menos 6 caracteres');
       return false;
     } else if (password.length > 20) {
-        Get.snackbar('Error', 'La contraseña debe tener menos de 20 caracteres');
-        return false;
-    }else if (!GetUtils.isEmail(email)) {
-        Get.snackbar('Error', 'El correo no es valido');
-        return false;
+      Get.snackbar('Error', 'La contraseña debe tener menos de 20 caracteres');
+      return false;
+    } else if (!GetUtils.isEmail(email)) {
+      Get.snackbar('Error', 'El correo no es valido');
+      return false;
     } else {
-          Get.snackbar('Success', 'Datos correctos');
-          return true;
+      Get.snackbar('Success', 'Datos correctos');
+      return true;
     }
   }
 
--- lib/src/providers/users_provider.dart
+++ lib/src/providers/users_provider.dart
@@ -1,4 +1,5 @@
 import 'package:death_dream/src/environments/environments.dart';
+import 'package:death_dream/src/models/response_api.dart';
 import 'package:death_dream/src/models/user.dart';
 import 'package:get/get.dart';
 
@@ -6,11 +7,20 @@ import 'package:get/get.dart';
 class UsersProvider extends GetConnect {
   String url = '${Enviroment.API_URL}api/users';
   Future<Response> create(User user) async {
-    Response response = await post(
-      '$url/create', 
-      user.toJson(),
-      headers: {'Content-Type': 'application/json'}
-    );
+    Response response = await post('$url/create', user.toJson(),
+        headers: {'Content-Type': 'application/json'});
     return response;
+  }
+
+  Future<ResponseApi> login(String email, String password) async {
+    Response response = await post(
+        '$url/login', {'email': email, 'password': password},
+        headers: {'Content-Type': 'application/json'});
+    if (response.body == null) {
+      Get.snackbar('Error', 'No fue posible realizar la petición');
+      return ResponseApi();
+    }
+    ResponseApi responseApi = ResponseApi.fromJson(response.body);
+    return responseApi;
   }
 }
